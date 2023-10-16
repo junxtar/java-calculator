25388eede830b849643b0a01ab6d375c7b5f45a4
junxtar <aa1870088@naver.com>
2023-10-16 11:31:36 +0900

    실행 코드 구현

--- CalculatorMisson/src/Main.java
+++ CalculatorMisson/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
    public static void main(String[] args) {
-      System.out.println("Hello world!");
+      Calculator calculator = new Calculator(new AddOperation());
+      calculator.turnOn();
+      calculator.calculator(calculator.getNumber(), 10);
+      calculator.init();
+      calculator.calculator(calculator.getNumber(), 30);
+
+      calculator.setOperation(new SubtractOperation());
+      calculator.calculator(calculator.getNumber(), 10);
+      calculator.calculator(calculator.getNumber(), 10);
+
+      calculator.setOperation(new MultiplyOperation());
+      calculator.calculator(calculator.getNumber(), 10);
+
+      calculator.setOperation(new DivideOperation());
+      calculator.calculator(calculator.getNumber(), 10);
+      calculator.calculator(calculator.getNumber(), 0);
+      calculator.calculator(calculator.getNumber(), 10);
+      calculator.init();
+      calculator.setOperation(new AddOperation());
+      calculator.calculator(calculator.getNumber(), 10);
+
+      calculator.turnOff();
    }
 }

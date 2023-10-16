c51e14e4a7fc11d09a2b24858d4547966f529646
junxtar <aa1870088@naver.com>
2023-10-16 11:31:13 +0900

    나누기 연산자 클래스 구현

--- CalculatorMisson/src/DivideOperation.java
+++ CalculatorMisson/src/DivideOperation.java
@@ -1,10 +1,6 @@
 public class DivideOperation extends AbstractOperation{
    @Override
    public double operate(double firstNumber, double secondNumber) {
-      if (secondNumber == 0) {
-         throw new ArithmeticException("0으로 나눌 수 없습니다.");
-      }
-
       return firstNumber / secondNumber;
    }
 }
